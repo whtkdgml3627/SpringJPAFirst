a366c5ec169eb0f42ea208f565c9215644c8e04b
jo <whtkdgml36271@gmail.com>
2023-07-03 16:56:49 +0900

    0703 react boot server todo api 개발

---
+++ src/main/java/org/zerock/j1/dto/TodoDTO.java
@@ -0,0 +1,12 @@
+package org.zerock.j1.dto;
+
+import lombok.Data;
+
+@Data
+public class TodoDTO {
+  
+  //validation 여기에 작성
+  private Long tno;
+  private String title;
+
+}
--- src/test/java/org/zerock/j1/repository/TodoRepositoryTests.java
+++ src/test/java/org/zerock/j1/repository/TodoRepositoryTests.java
@@ -1,8 +1,10 @@
 package org.zerock.j1.repository;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
@@ -10,12 +12,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.zerock.j1.domain.Todo;
+import org.zerock.j1.dto.TodoDTO;
 
 import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
 @Log4j2
 public class TodoRepositoryTests {
+
+  @Autowired
+  private ModelMapper modelMapper;
 
   @Autowired
   private TodoRepository todoRepository;
@@ -30,6 +36,25 @@ public class TodoRepositoryTests {
       Todo result = todoRepository.save(todo);
       log.info(result);
     });
+
+  }
+
+  @Test
+  public void testRead(){
+
+    Long tno = 100L;
+
+    Optional<Todo> result = todoRepository.findById(tno);
+    
+    Todo entity = result.orElseThrow();
+
+    log.info("ENTITY-----------------------------");
+    log.info(entity);
+
+    //map(소스, 타입)
+    //modelMapper 라이브러리로 Entity -> DTO로 변환
+    TodoDTO dto = modelMapper.map(entity, TodoDTO.class);
+    log.info(dto);
 
   }
 
